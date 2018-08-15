f18daa154def37146ae2dc610e588b0be50ae4d6
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-08-15 14:58:39 +0700

    #child
    Từ parent khi bấm button thì thay đổi value ở child

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -3,19 +3,22 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
     selector: 'app-child',
     template: `
-        <button (click)="addForParent();">Add</button>
-        <button (click)="subForParent();">Sub</button>
+        <!-- <button (click)="addForParent();">Add</button> -->
+        <!-- <button (click)="subForParent();">Sub</button> -->
+        <h3>{{ value }}</h3>
     `
 })
 
 export class ChildComponent {
-    @Output() myClick = new EventEmitter<boolean>();
+    // @Output() myClick = new EventEmitter<boolean>();
 
-    addForParent() {
-        this.myClick.emit(true);
-    }
+    // addForParent() {
+    //     this.myClick.emit(true);
+    // }
 
-    subForParent() {
-        this.myClick.emit(false);
-    }
+    // subForParent() {
+    //     this.myClick.emit(false);
+    // }
+
+    value = 0;
 }
--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -3,19 +3,21 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-parent',
     template: `
-        <h3>{{ value }}</h3>
-        <app-child (myClick)="changeValue($event);"></app-child>
+        <!-- <h3>{{ value }}</h3> -->
+        <!-- <app-child (myClick)="changeValue($event);"></app-child> -->
+        <button (click)="child.value = child.value + 1">Add for child</button>
+        <app-child #child></app-child>
     `
 })
 
 export class ParentComponent {
-    value = 0;
+    // value = 0;
 
-    changeValue(isAdd: boolean) {
-        if (isAdd) {
-            this.value = this.value + 1;
-        } else {
-            this.value = this.value - 1;
-        }
-    }
+    // changeValue(isAdd: boolean) {
+    //     if (isAdd) {
+    //         this.value = this.value + 1;
+    //     } else {
+    //         this.value = this.value - 1;
+    //     }
+    // }
 }
