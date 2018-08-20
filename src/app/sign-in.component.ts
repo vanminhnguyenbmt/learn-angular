b13028279ad1b4c3985ee6ca1fa7a6add6e2c2d6
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-08-20 11:43:17 +0700

    Add styles for input invalid

--- src/app/sign-in.component.ts
+++ src/app/sign-in.component.ts
@@ -3,21 +3,27 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-sign-in',
     template: `
-        <form (ngSubmit)="onSubmit(formSignIn);" #formSignIn="ngForm">
+        <form (ngSubmit)="onSubmit(txtEmail);" #formSignIn="ngForm">
             <input
                 placeholder="Email"
                 ngModel
+                #txtEmail="ngModel"
                 name="email"
                 required
+                email
             >
-            <p *ngIf="formSignIn.controls.email?.errors?.required">
+            <p *ngIf="txtEmail.touched && txtEmail.errors?.required">
                 Email is required
+            </p>
+            <p *ngIf="txtEmail.errors?.email">
+                Email is not valid
             </p>
             <br><br>
             <input type="password" placeholder="Password" ngModel name="password">
             <br><br>
             <button [disabled]="formSignIn.invalid">Submit</button>
         </form>
+        <p>{{ txtEmail.errors | json }}</p>
     `
 })
 
