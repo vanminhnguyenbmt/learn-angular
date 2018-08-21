4fa9e2e4085a19426b46077857913c8ddba6cb65
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-08-21 09:52:09 +0700

    Routing with params

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
-
-  constructor() { }
+  name = '';
+  phoneNumber = '';
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const id = params.get('id');
+      this.name = params.get('name');
+      this.phoneNumber = params.get('phoneNumber');
+    });
   }
 
 }
